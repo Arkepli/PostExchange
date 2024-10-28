ec01412b8256e957eb2da69ba0d0bcc1201589db
David Santiago Bonilla Achury <david_bonillaac@fet.edu.co>
2024-10-28 13:41:05 -0500

    GET and POST Requests

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const db = require("./models");
+app.use(express.json()); // To parse the incoming requests with JSON payloads
+
+const posts = require("./routes/Posts");
+app.use("/posts", posts);
+
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
