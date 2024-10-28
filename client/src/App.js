7840197e735a5f03039753da46bd95459cf58329
David Santiago Bonilla Achury <david_bonillaac@fet.edu.co>
2024-10-28 16:18:59 -0500

    Form Validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,19 @@
 import "./App.css";
-import axios from "axios";
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePosts from "./pages/CreatePosts";
 
 function App() {
-  const [listOfPosts, setListOfPosts] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:3001/posts")
-      .then((response) => {
-        console.log(response.data);
-        setListOfPosts(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
   return (
     <div className="App">
-      {listOfPosts.map((value, key) => {
-        return (
-        <div className="post">
-          <div className="title">{value.title}</div>
-          <div className="body">{value.postText}</div>
-          <div className="footer">{value.username}</div>
-        </div>
-      );
-      })}
+      <Router>
+        <Link to="/CreatePost">Create A Post</Link>
+        <Link to="/"> Home Page</Link>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/CreatePost" element={<CreatePosts />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
