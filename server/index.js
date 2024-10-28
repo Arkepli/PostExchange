1f2a9d62a5e5a2f1e1c5f8ba063f4718765793d0
David Santiago Bonilla Achury <david_bonillaac@fet.edu.co>
2024-10-28 14:43:09 -0500

    Connecting ReactJS to NodeJS

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
-const db = require("./models");
+const cors = require("cors");
+
 app.use(express.json()); // To parse the incoming requests with JSON payloads
+app.use(cors());  // To allow cross-origin requests
+
+const db = require("./models");
 
 const posts = require("./routes/Posts");
 app.use("/posts", posts);
-
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
