5aba66a7179766fe4ac720f193189d9338b92873
David Santiago Bonilla Achury <david_bonillaac@fet.edu.co>
2024-10-29 11:15:24 -0500

    Registration and Login

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,25 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePosts from "./pages/CreatePosts";
 import Post from "./pages/Post";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <div className="navbar">
-        <Link to="/CreatePost">Create A Post</Link>
-        <Link to="/"> Home Page</Link>
+        <Link to="/CreatePost"> Create A Post </Link>
+        <Link to="/"> Home Page </Link>
+        <Link to="/login"> Login </Link>
+        <Link to="/registration"> Registration </Link>
         </div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/CreatePost" element={<CreatePosts />} />
           <Route path="/Post/:id" element={<Post />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registration" element={<Registration />} />
         </Routes>
       </Router>
     </div>
